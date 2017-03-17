c6a1617462de9e45e62b5c78f825d3569fbbc2a2
Matyldzia22 <mpyszynska@gmail.com>
2017-03-17 01:10:23 +0100

    some data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Build.create(name:'Lotus Temple', country:'India', style:'Expressionist', photo:'http://s3.india.com/travel/wp-content/uploads/2016/03/Lotus-temple-main.jpg')
+Build.create(name:'Cologne Cathedral', country:'Germany', style:'Gothic', photo:'http://static.thousandwonders.net/Cologne.Cathedral.original.22182.jpg')
+Build.create(name:'Dome of the Rock', country:'Jerusalem', style:'Umayyad',photo:'http://www.bibleplaces.com/wp-content/uploads/2015/07/Dome-of-the-Rock-from-south-tb091306191-bibleplaces.jpg')
+Build.create(name:'La Pedrera', country:'Spain', style:'Secession',photo:'https://www.hotelmajestic.es/sites/default/files/casa-mila-la-pedrera-hotel-majestic-5-estrellas-barcelona.jpg')
+Build.create(name:'One World Trade Center', country:'USA', style:'modern',photo:'https://timedotcom.files.wordpress.com/2014/03/world-trade-center.jpg')
+Build.create(name:'St Pauls Cathedral', country:'United Kingdom', style:'Baroque', photo:'https://www.stpauls.co.uk/SM4/Mutable/Uploads/generic_image/DP073155_8231538.jpg')
+Build.create(name:'Petronas Towers', country:'Malaysia', style:'Postmodern', photo:'https://www.askideas.com/media/37/Day-Time-View-Of-Petronas-Towers.jpg')
+Build.create(name:'White House', country:'USA', style:'Neoclassical', photo:'http://cdn.history.com/sites/2/2013/11/district-of-columbia-white-house.jpg')
